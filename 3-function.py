b6268c60412a295781887b7bbec337aa0ddf7589
v-ivanko <v.ivanko@ubki.ua>
2020-05-24 11:24:03 +0300

    commit 3

---
+++ 3-function.py
@@ -0,0 +1,10 @@
+x = int(input("\nВведите 1 аргумент: "))
+y = int(input("Введите 2 аргумент: "))
+
+def func(x,y):
+    return v
+v=x*y
+def func2(z):
+    return z
+z=v
+print("\nРезультат умножения: " + str(z))
