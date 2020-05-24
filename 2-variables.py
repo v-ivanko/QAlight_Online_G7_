a376bcc8ab2c8292d1de1406d77f5d49f6844ae1
v-ivanko <v.ivanko@ubki.ua>
2020-05-24 11:20:55 +0300

    Initial commit

---
+++ 2-variables.py
@@ -0,0 +1,24 @@
+#string
+print("string:")
+a = "one"
+b = "two"
+c = a+" "+b
+print(c)
+
+#integer
+print(" \ninteger:")
+a=15
+b=7
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#float
+print(" \nfloat:")
+a=10.3
+b=28.7
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
